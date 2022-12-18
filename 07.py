c486d35b5327ff41f4815c035b3e5c3a4f17f481
Shivam Kumar <114799228+kumarshivam27december@users.noreply.github.com>
2022-12-19 00:16:23 +0530

    Update 07.py

--- 07.py
+++ 07.py
@@ -1,3 +1,12 @@
+'''we just have to search 
+if 200 is present in dict
+which is not present so we 
+will search the word related to
+which if it is present or not 
+for example in this problem b
+is related to 200 since b is
+present it will give output
+200 is present in the dictionary'''
 sample_dict = {'a': 100, 'b': 200, 'c': 300}
 if 'b' in sample_dict:
     print("200 is present in the dictionary")
