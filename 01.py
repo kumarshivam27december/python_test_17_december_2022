6e95dc915074e3e5b0cc7417306d26f871dbfbda
Shivam Kumar <114799228+kumarshivam27december@users.noreply.github.com>
2022-12-19 00:10:05 +0530

    Update 01.py

--- 01.py
+++ 01.py
@@ -1,11 +1,20 @@
 k=['ten','twenty','thirty']
 v=[10,20,30]
-# print(dict())
+
+
+
 k.insert(1,v[0])
 k.insert(3,v[1])
 k.insert(5,v[2])
 print(k)
+
+
+
+
 def cnvrt(k):
     res_dct={k[i]: k[i+1] for i in range(0,len(k),2)}
     return res_dct
 print(cnvrt(k))
+
+
+
